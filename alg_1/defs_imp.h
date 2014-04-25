d282c7dc3143f48b267aa85774010b8c4c7c89ec
grahamjpark <cs240@CS240.(none)>
2014-04-25 10:07:30 -0400

    Working on revising the old libraries

---
+++ alg_1/defs_imp.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <math.h>
+
+typedef struct song {
+	char* name;
+	//Length of song in seconds
+	double len;
+	//TODO: Implement rating weight. Will try to optimize to find most likeable playlist
+	//int rating;
+} Song;
