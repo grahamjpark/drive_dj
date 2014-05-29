aae9a7146c3e995bba23df7673ba10c1ecb65f7e
grahamjpark <cs240@CS240.(none)>
2014-05-28 23:33:37 -0400

    Testing new names

---
+++ alg_1/read_args.h
@@ -0,0 +1,33 @@
+typedef struct table {
+	// Number of args in table (also position where next element will go)
+	int pos;
+	// Arrays of keys with their coresponding values
+	char** keys;
+	char** values;
+} Table;
+
+/*
+ * Read and parse command line arguments. argv is an array of argc strings.
+ * The format is "-key" or "-key=val" where "key" and "val" are sequences of
+ * alpha-numeric characters. All other string are silently discarded.  For
+ * any key, only its last definition is retained.  "-key" has a NULL value.
+ */
+Table* ca_init(int argc, char **argv);
+
+/*
+ * Check is the null-terminated string key is defined. Answer true if either
+ * "-key" or "-key=val" were passed at the command line.
+ */
+int ca_defined(char *key, Table* tbl);
+
+/*
+ * If "key" is associated to "val", returns atoi(val). If "val" is NULL,
+ * returns -1.  Undefined behavior, if "key" is not found.
+ */
+int ca_int_value(char *key, Table* tbl);
+
+/*
+ * If "key" is associated to "val", returns val. Undefined behavior, if
+ * "key" is not found.
+ */
+char *ca_str_value(char *key, Table* tbl);
